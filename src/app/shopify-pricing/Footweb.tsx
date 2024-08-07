f66bae1530c4e3d1521d9d36d894b9e0acd8c825
brijeshbase2brand <brijeshbase2brand@gmail.com>
2024-08-07 13:44:33 +0530

    price page design

---
+++ src/app/shopify-pricing/Footweb.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Footweb = () => {
+  return (
+    <>
+      <section className="pt-lg-5 pt-4 pb-lg-5 pb-4 b2b-gray-bg ">
+        <div className="b2b-container-lg">
+          <div className="last-gk">
+            <h2 className="gk-sub-heading-bold text-center text-uppercase text-cente m-auto gk-width">
+              Shopify Website Development Service
+            </h2>
+            <p className="b2b-sub-text text-center  mt-4 ">
+              At Base2Brand, we specialize in creating Shopify websites that are
+              tailored to meet the unique needs of our clients. From design and
+              development to optimization and launch, we handle every aspect of
+              the website development process with a focus on delivering
+              exceptional results.
+            </p>
+            <p className=" b2b-sub-text text-center mt-4 ">
+              Our team of experts is committed to staying up-to-date with the
+              latest trends and technologies in ecommerce to ensure that our
+              clients are always ahead of the curve. Whether you’re looking to
+              launch a new Shopify store or revamp an existing one, we have the
+              skills and expertise to help you achieve your goals.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Footweb;
